ed1bd39e6f1e7b0f419317b02c9e269e9b62045e
k.c. schroeter <kesc0188@colorado.edu>
2017-06-27 09:07:49 -0400

    Update nomenclature & clean

--- maka.js
+++ maka.js
@@ -1,105 +1,32 @@
-// maka.js - part of make america kittens again
-// v1.2.1
-// by Tom Royal
-// tomroyal.com
 
-//var makaTesting = false; // for debugging only
-
-//if (makaTesting){
-//	console.log('maka initiated');
-//	var makaReplacements = 0;
-//}
-
-// init blacklist
-
-//var blacklist = ["trump", "трамп", "トランプ"]; // thanks to jSanchoDev and akiatoji for translations
-
-// get additional settings from chrome storage
-/*
-chrome.storage.local.get({
-    blockPence: false,
-    blockFarage: false,
-    blockLePen: false,
-    blockWilders: false,
-    blockBannon: false,
-    customBlock: false
-  }, function(items) {
-	  if (items.blockPence){
-		  blacklist.push("mike pence");
-		  blacklist.push("ペンス");
-	  };
-	  if (items.blockFarage){
-		  blacklist.push("farage");
-	  };
-	  if (items.blockLePen){
-		  blacklist.push("le pen");
-	  };
-	  if (items.blockWilders){
-		  blacklist.push("wilders");
-	  };
-	  if (items.blockBannon){
-		  blacklist.push("bannon");
-	  };
-	  // process custom blocklist
-
-	  if(items.customBlock){
-			var customBlockTargets = items.customBlock.split(',');
-			  customBlockTargets.forEach(function(blockTarget) {
-				    blacklist.push(blockTarget.trim().toLowerCase())
-			  });
-	  };
-
-	  document.addEventListener('DOMContentLoaded', makanow(theLeos), false);
-
-  });
-*/
 chrome.storage.local.get(
 	  function(){
-		document.addEventListener( 'DOMContentLoaded', makanow(theLeos), false);
+		document.addEventListener( 'DOMContentLoaded', leonow(theLeos), false);
 });
-// kitten data!
-// Note - now moved from S3 to local storage
-
 
 var theLeos = {"leo": [
- {"file": "1.jpg", "Credit": "Crsan", "URL": "http://www.flickr.com/photos/crsan/2571204498/", "type":"0"},
-	{"file": "2.jpg", "Credit": "Abcrumley", "URL": "http://www.flickr.com/photos/crumley/160490011/", "type":"0"},
-	{"file": "3.jpg", "Credit": "Woodchild2010", "URL": "http://www.flickr.com/photos/woodchild/5335939044/", "type":"0"},
-	{"file": "4.jpg", "Credit": "Vancouverfilmschool", "URL": "http://www.flickr.com/photos/vancouverfilmschool/4838552777/", "type":"0"},
-	{"file": "5.jpg", "Credit": "Jameswragg", "URL": "http://www.flickr.com/photos/jameswragg/4688532009/", "type":"0"},
-	{"file": "6.jpg", "Credit": "Eva101", "URL": "http://www.flickr.com/photos/evapro/519752551/", "type":"0"},
-	{"file": "7.jpg", "Credit": "Pinguino", "URL": "http://www.flickr.com/photos/pinguino/2655478691/", "type":"0"},
-	{"file": "8.jpg", "Credit": "Daisyree Bakker", "URL": "http://www.flickr.com/photos/27875041@N02/4710868953/", "type":"0"},
-	{"file": "9.jpg", "Credit": "VictoriaPeckham", "URL": "http://www.flickr.com/photos/victoriapeckham/4000992556/", "type":"0"},
-	{"file": "10.jpg", "Credit": "Peter Huys", "URL": "http://www.flickr.com/photos/darksidex/4568967536/", "type":"0"},
-	{"file": "11.jpg", "Credit": "Lauren Nelson", "URL": "http://www.flickr.com/photos/lulieboo/3523637733/", "type":"0"},
-	{"file": "12.jpg", "Credit": "Denizen24", "URL": "http://www.flickr.com/photos/39311243@N05/4273391516/", "type":"0"},
-	{"file": "13.jpg", "Credit": "Pinguino", "URL": "http://www.flickr.com/photos/pinguino/2655477765/", "type":"0"},
-	{"file": "14.jpg", "Credit": "Sikander", "URL": "http://www.flickr.com/photos/sikander/3941418808/", "type":"0"},
-	{"file": "15.jpg", "Credit": "Snickclunk", "URL": "ttp://www.flickr.com/photos/snickclunk/4985501252/", "type":"0"},
-	{"file": "16.jpg", "Credit": "JeffreyW", "URL": "http://www.flickr.com/photos/jeffreyww/4975375328/", "type":"0"},
-	{"file": "17.jpg", "Credit": "Jeremy Bronson", "URL": "http://www.flickr.com/photos/jbrons/4872001139/", "type":"0"}
-	//{"file": "18.jpg", "Credit": "Threat to Democracy", "URL": "http://www.flickr.com/photos/16725630@N00/4811658578/", "type":"0"},
-	//{"file": "19.jpg", "Credit": "Eofstr", "URL": "http://www.flickr.com/photos/eofstr/4757862779/", "type":"0"},
-	//{"file": "20.jpg", "Credit": "DvdOuden", "URL": "http://www.flickr.com/photos/dvdouden/4733485066/", "type":"0"},
-	//{"file": "21.jpg", "Credit": "Cuttlefish", "URL": "http://www.flickr.com/photos/cuttlefish/4969726052/", "type":"0"},
-	//{"file": "22.jpg", "Credit": "JenniferC", "URL": "http://www.flickr.com/photos/29638108@N06/5104339654/", "type":"0"},
-	//{"file": "23.jpg", "Credit": "Jasontoff", "URL": "http://www.flickr.com/photos/jasontoff/5098333343/", "type":"0"},
-	//{"file": "24.jpg", "Credit": "FHgitarre", "URL": "http://www.flickr.com/photos/48356868@N06/5073501988/", "type":"0"},
-	//{"file": "25.jpg", "Credit": "Phil Hawksworth", "URL": "http://www.flickr.com/photos/philhawksworth/5037670666/", "type":"0"},
-	//{"file": "26.jpg", "Credit": "Phil Hawksworth", "URL": "http://www.flickr.com/photos/philhawksworth/5036995211/", "type":"0"},
-	//{"file": "27.jpg", "Credit": "FragmentFi", "URL": "http://www.flickr.com/photos/fragmentfi/5033666682/", "type":"0"},
-	//{"file": "28.jpg", "Credit": "SpookyPeanut", "URL": "http://www.flickr.com/photos/spookypeanut/5502011850/", "type":"0"},
-	//{"file": "29.jpg", "Credit": "Glennsajan", "URL": "http://www.flickr.com/photos/glennsajan/5485364346/", "type":"0"},
-	//{"file": "30.jpg", "Credit": "Woodchild2010", "URL": "http://www.flickr.com/photos/woodchild/5335939044/", "type":"0"},
-	//{"file": "31.jpg", "Credit": "Ollie Crafoord", "URL": "http://www.flickr.com/photos/lollaping/5277362546/", "type":"0"},
-	//{"file": "32.jpg", "Credit": "Mami Terai", "URL": "http://www.google.com", "type":"1"},
-	//{"file": "33.jpg", "Credit": "Sam Scheibel", "URL": "http://www.google.com", "type":"1"}
+ {"file": "1.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "2.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "3.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "4.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "5.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "6.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "7.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "8.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "9.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "10.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "11.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "12.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "13.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "14.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "15.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "16.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"},
+	{"file": "17.jpg", "Credit": "Leo", "URL": "http://www.leo.org", "type":"0"}
     ]
 };
 
-function makanow(theLeos){
-	// called on page load. Searches all img alt text and srcs for the strings in blacklist, replaces with kittens
+function leonow(theLeos){
+	// called on page load. 
 	var pagepics=document.getElementsByTagName("img"), i=0, img;
 	while (img = pagepics[i++])
 	{
@@ -108,9 +35,9 @@ function makanow(theLeos){
 			var imgheight = img.clientHeight;
 
 					// append old src
-					img.setAttribute("makareplaced", img.src);
+					img.setAttribute("leoreplaced", img.src);
 
-					// remove srcsets, forcing browser to the kitten - eg, BBC News
+					// remove srcsets, forcing browser to the leo - eg, BBC News
 					if (img.hasAttribute('srcset')){
 						img.removeAttribute('srcset');
 					};
@@ -149,32 +76,16 @@ function makanow(theLeos){
 					else {
 						img.alt = 'Photo by '+theLeos.leo[randk].Credit+'';
 					};
-//					makaReplacements++;
-
-
-
 	}
-//	if (makaTesting){
-//		console.log('maka processing complete, replaced '+makaReplacements+' images');
-//	}
 };
 
-// function to replace kittened-images with the original SRCs
-
-function undomakanow(){
-//	if (makaTesting){
-//		console.log('undoing MAKA');
-//	}
-
+function undoleonow(){
 	var pagepics=document.getElementsByTagName("img"), i=0, img;
 	while (img = pagepics[i++])
 	{
-		if (img.hasAttribute('makareplaced')){
-//			if (makaTesting){
-//				console.log('replacing image');
-//			};
-			img.src = img.getAttribute('makareplaced');
-			img.removeAttribute('makareplaced');
+		if (img.hasAttribute('leoreplaced')){
+			img.src = img.getAttribute('leoreplaced');
+			img.removeAttribute('leoreplaced');
 		};
 	};
 
@@ -183,13 +94,8 @@ function undomakanow(){
 // listener for context menu click invoking the above
 
 chrome.extension.onMessage.addListener(function (message, sender, callback) {
-    if (message.functiontoInvoke == "undoMAKA") {
+    if (message.functiontoInvoke == "undoLEO") {
 	    // undo function called
         undomakanow();
     };
-    /*
-    else if (message.functiontoInvoke == "redoMAKA") {
-        makanow(theKittens);
-    }
-    */
 });
