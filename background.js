41a727b5e734855e51b7c45ca25bba19a4d5380a
k.c. schroeter <kesc0188@colorado.edu>
2017-06-29 12:51:16 -0400

    Add switch

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "runLeos.js"});
+var picChange = true;
+
+chrome.browserAction.onClicked.addListener(function(info,tab) {
+   chrome.tabs.query({
+      "active": true, 
+      "currentWindow": true
+   }, function (tabs) {
+//   chrome.tabs.executeScript(null, {file: "runLeos.js"});
+      chrome.tabs.sendMessage(tabs[0].id, {
+         "functiontoInvoke": "changePics"
+      });
+   });
+   
+   if (picChange == true) {
+      picChange = false; 
+   }
+   else {
+      picChange = true;
+   }
+   
 });
